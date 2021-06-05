12766dfd21007e0f068945741dd913d3498aa655
valgolei <84638017+valgolei@users.noreply.github.com>
2021-06-05 16:18:41 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,29 +2,47 @@ namespace SpriteKind {
     export const tir_ami = SpriteKind.create()
     export const ennemi_vivant = SpriteKind.create()
     export const projectil_de_la_mort = SpriteKind.create()
+    export const tir_ennemi = SpriteKind.create()
+    export const fruit = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.tir_ami, SpriteKind.ennemi_vivant, function (sprite, otherSprite) {
     sprite.destroy()
     statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -34
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.tir_ennemi, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    vie.value += -15
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.fruit, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    energie.value += 40
 })
 sprites.onOverlap(SpriteKind.projectil_de_la_mort, SpriteKind.ennemi_vivant, function (sprite, otherSprite) {
     sprite.destroy()
     statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -200
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    tir_joueur = sprites.createProjectileFromSprite(img`
-        . . . 
-        . 2 . 
-        . 2 . 
-        . 2 . 
-        . 2 . 
-        . . . 
-        `, vaisseau, 0, -100)
-    tir_joueur.setKind(SpriteKind.tir_ami)
+    if (energie.value != 0) {
+        tir_joueur = sprites.createProjectileFromSprite(img`
+            . . . 
+            . 2 . 
+            . 2 . 
+            . 2 . 
+            . 2 . 
+            . . . 
+            `, vaisseau, 0, -100)
+        tir_joueur.setKind(SpriteKind.tir_ami)
+        energie.value += -3
+    }
 })
 statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
     status.spriteAttachedTo().destroy(effects.confetti, 100)
     magie.value += 17
+    xp += 1
+})
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    pause(100)
+    game.over(false)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (magie.value == 100) {
@@ -42,25 +60,23 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.ennemi_vivant, function (sprite, otherSprite) {
-    statusbar.value += -34
+    vie.value += -34
     otherSprite.destroy()
-})
-statusbars.onZero(StatusBarKind.Energy, function (status) {
-    pause(100)
-    game.over(false)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    statusbar.value += -34
+    vie.value += -34
     otherSprite.destroy()
 })
-let projectile_ennemi: Sprite = null
-let statusbar2: StatusBarSprite = null
+let pomme: Sprite = null
+let vie_ennemi: StatusBarSprite = null
 let ennemi_b: Sprite = null
 let ennemi_a: Sprite = null
+let projectile_ennemi: Sprite = null
 let projectile_de_la_mort_ami: Sprite = null
 let tir_joueur: Sprite = null
+let energie: StatusBarSprite = null
 let magie: StatusBarSprite = null
-let statusbar: StatusBarSprite = null
+let vie: StatusBarSprite = null
 let vaisseau: Sprite = null
 effects.starField.startScreenEffect()
 vaisseau = sprites.create(img`
@@ -83,11 +99,40 @@ vaisseau = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(vaisseau)
 vaisseau.setStayInScreen(true)
-statusbar = statusbars.create(12, 3, StatusBarKind.Energy)
-statusbar.attachToSprite(vaisseau, -18, 0)
+vie = statusbars.create(12, 3, StatusBarKind.Health)
+vie.attachToSprite(vaisseau, -18, 0)
 magie = statusbars.create(25, 4, StatusBarKind.Magic)
 magie.setPosition(16, 6)
-game.onUpdateInterval(5000, function () {
+energie = statusbars.create(20, 4, StatusBarKind.Energy)
+energie.setPosition(12, 115)
+let xp = 0
+game.onUpdateInterval(2000, function () {
+    if (energie.value == 0) {
+        vie.value += -4
+    }
+    if (energie.value > 50) {
+        vie.value += 2
+    }
+    if (energie.value == 100) {
+        vie.value += 2
+    }
+})
+game.onUpdateInterval(1000, function () {
+    for (let xp of sprites.allOfKind(SpriteKind.ennemi_vivant)) {
+        if (Math.percentChance(50)) {
+            projectile_ennemi = sprites.createProjectileFromSprite(img`
+                . . . 
+                . 2 . 
+                . 2 . 
+                . 2 . 
+                . 2 . 
+                . . . 
+                `, xp, 0, 70)
+            projectile_ennemi.setKind(SpriteKind.tir_ennemi)
+        }
+    }
+})
+game.onUpdateInterval(6000, function () {
     ennemi_a = sprites.createProjectileFromSide(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -108,8 +153,6 @@ game.onUpdateInterval(5000, function () {
         `, 0, 74)
     ennemi_a.x = randint(5, 155)
     ennemi_a.setKind(SpriteKind.Enemy)
-})
-game.onUpdateInterval(5000, function () {
     ennemi_b = sprites.createProjectileFromSide(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -130,23 +173,52 @@ game.onUpdateInterval(5000, function () {
         `, 0, 5)
     ennemi_b.x = randint(5, 155)
     ennemi_b.setKind(SpriteKind.ennemi_vivant)
-    statusbar2 = statusbars.create(8, 1, StatusBarKind.EnemyHealth)
-    statusbar2.attachToSprite(ennemi_b, -2, 0)
+    vie_ennemi = statusbars.create(8, 1, StatusBarKind.EnemyHealth)
+    vie_ennemi.attachToSprite(ennemi_b, -2, 0)
 })
-game.onUpdateInterval(500, function () {
-    for (let valeur of sprites.allOfKind(SpriteKind.ennemi_vivant)) {
-        if (Math.percentChance(50)) {
-            projectile_ennemi = sprites.createProjectileFromSprite(img`
-                . . . 
-                . 2 . 
-                . 2 . 
-                . 2 . 
-                . 2 . 
-                . . . 
-                `, valeur, 0, 70)
-        }
-    }
+game.onUpdateInterval(, function () {
+    ennemi_b = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 1 . . . . 1 . . . . . 
+        . . . . . d d d d d d . . . . . 
+        . . . . . d . 9 9 . d . . . . . 
+        . . . . . d . . . . d . . . . . 
+        . . . . . 2 . . . . 2 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 0, 5)
+    ennemi_b.x = randint(5, 155)
+    ennemi_b.setKind(SpriteKind.ennemi_vivant)
+    vie_ennemi = statusbars.create(8, 1, StatusBarKind.EnemyHealth)
+    vie_ennemi.attachToSprite(ennemi_b, -2, 0)
 })
 game.onUpdateInterval(10000, function () {
-    statusbar.value += 10
+    pomme = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . 7 . . . . . . 
+        . . . . . . . 7 . . . . . . . 
+        . . . . . . 2 2 2 . . . . . . 
+        . . . . . 2 2 1 2 2 . . . . . 
+        . . . . . 2 2 2 1 2 . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . 
+        . . . . . . 2 2 2 . . . . . . 
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 
+        `, 0, 60)
+    pomme.x = randint(5, 155)
+    pomme.setKind(SpriteKind.fruit)
 })
