a850f3c2266b2539c7186c37bf9880312b92807a
valgolei <84638017+valgolei@users.noreply.github.com>
2024-01-24 22:20:00 +0100

    Mise à jour main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -181,7 +181,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
     status.spriteAttachedTo().destroy()
     magie.value += 17
-    info.changeScoreBy(10)
+    info.changeScoreBy(60)
 })
 sprites.onOverlap(SpriteKind.projectil_de_la_mort, SpriteKind.BOSS, function (sprite, otherSprite) {
     music.bigCrash.play()
